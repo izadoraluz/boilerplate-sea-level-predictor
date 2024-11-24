218175aa42136d53bfbb445f9a0890bc0ddb8c90
Izadora Luz <izadora.novaes@sou.inteli.edu.br>
2024-11-24 19:01:20 +0000

    feat: previsor da água do mar

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -3,21 +3,35 @@ import matplotlib.pyplot as plt
 from scipy.stats import linregress
 
 def draw_plot():
-    # Read data from file
+    # Leia os dados
+    df = pd.read_csv('epa-sea-level.csv')
 
+    # Criar a figura e o eixo
+    fig, ax = plt.subplots(figsize=(10, 6))
 
-    # Create scatter plot
+    # Criar o gráfico de dispersão
+    ax.scatter(df['Year'], df['CSIRO Adjusted Sea Level'], color='blue', label='Data')
 
+    # Linha de melhor ajuste para todos os dados
+    slope, intercept, _, _, _ = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
+    years_extended = pd.Series(range(1880, 2051))
+    sea_level_predicted = slope * years_extended + intercept
+    ax.plot(years_extended, sea_level_predicted, color='red', label='Fit: All Data')
 
-    # Create first line of best fit
+    # Linha de melhor ajuste desde o ano 2000
+    df_recent = df[df['Year'] >= 2000]
+    slope_recent, intercept_recent, _, _, _ = linregress(df_recent['Year'], df_recent['CSIRO Adjusted Sea Level'])
+    years_recent_extended = pd.Series(range(2000, 2051))
+    sea_level_recent_predicted = slope_recent * years_recent_extended + intercept_recent
+    ax.plot(years_recent_extended, sea_level_recent_predicted, color='green', label='Fit: From 2000')
 
+    # Configurações do gráfico
+    ax.set_title('Rise in Sea Level')
+    ax.set_xlabel('Year')
+    ax.set_ylabel('Sea Level (inches)')
+    ax.legend()
+    ax.grid(True)
 
-    # Create second line of best fit
-
-
-    # Add labels and title
-
-    
-    # Save plot and return data for testing (DO NOT MODIFY)
+    # Salvar o gráfico
     plt.savefig('sea_level_plot.png')
-    return plt.gca()
+    return ax
